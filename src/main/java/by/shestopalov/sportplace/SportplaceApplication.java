efc0001ba2a4ba9fc62486f1ccec545a170f2b8a
Denis Shestopalov <KROTIK96@gmail.com>
2020-09-24 08:35:51 +0300

    Initial commit

---
+++ src/main/java/by/shestopalov/sportplace/SportplaceApplication.java
@@ -0,0 +1,13 @@
+package by.shestopalov.sportplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/shestopalov/sportplace/SportplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shestopalov.sportplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
